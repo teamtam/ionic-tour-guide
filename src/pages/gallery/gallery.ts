171c3431f8e867a1549b8db39e62eb81f28b9cee
Benjamin Tam <ben@teamtam.com>
2017-06-19 16:16:01 +1000

    Load images from gallery

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-gallery',
-  templateUrl: 'gallery.html'
+  templateUrl: 'gallery.html',
+  providers: [Camera]
 })
 export class GalleryPage {
 
-  constructor(public navCtrl: NavController) {
+  private imageSrc: string;
 
+  constructor(public navCtrl: NavController, private camera: Camera) {
+
+  }
+
+  private openGallery (): void {
+    let cameraOptions = {
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.camera.EncodingType.JPEG,
+      correctOrientation: true
+    }
+
+    this.camera.getPicture(cameraOptions)
+      .then(file_uri => this.imageSrc = file_uri,
+      err => console.log(err));
   }
 
 }
