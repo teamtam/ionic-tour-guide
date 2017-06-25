b30098d9719396228379bd3327812a01d2d5be42
Benjamin Tam <ben@teamtam.com>
2017-06-25 23:58:07 +1000

    Takes a photo

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-camera',
-  templateUrl: 'camera.html'
+  templateUrl: 'camera.html',
+  providers: [Camera]
 })
 export class CameraPage {
 
-  constructor(public navCtrl: NavController) {
+  public base64Image: string;
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  takePicture() {
+    this.camera.getPicture({
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
