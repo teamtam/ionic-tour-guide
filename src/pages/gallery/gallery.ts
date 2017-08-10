a3eef39f22011f972a0dbadc17c6923ad435605b
Benjamin Tam <ben@teamtam.com>
2017-08-10 22:50:01 +1000

    Use photo gallery plugin instead of camera

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,45 +1,60 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavController, Platform, ToastController, ModalController  } from 'ionic-angular';
+import { PhotoLibrary, LibraryItem } from '@ionic-native/photo-library';
+
+import { HomePage } from '../home/home';
+
+const THUMBNAIL_WIDTH = 300;
+const THUMBNAIL_HEIGHT = 300;
 
 @Component({
   selector: 'page-gallery',
   templateUrl: 'gallery.html',
-  providers: [Camera]
+  providers: [PhotoLibrary]
 })
+
 export class GalleryPage {
 
-  private imageSrc: string;
-  private imageSelected: boolean = false;
+  thumbnailWidth = THUMBNAIL_WIDTH + 'px';
+  thumbnailHeight = THUMBNAIL_HEIGHT + 'px';
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  library: LibraryItem[];
 
+  constructor(public navCtrl: NavController, private photoLibrary: PhotoLibrary, private platform: Platform,
+    private cd: ChangeDetectorRef, private toastCtrl: ToastController, private modalCtrl: ModalController) {
+
+    this.library = [];
+    this.fetchPhotos();
   }
 
-  private openGallery (): void {
-    let cameraOptions = {
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      quality: 100,
-      targetWidth: 1000,
-      targetHeight: 1000,
-      encodingType: this.camera.EncodingType.JPEG,
-      correctOrientation: true
-    }
+  fetchPhotos() {
 
-    this.camera.getPicture(cameraOptions)
-      .then(
-        (file_uri) => {
-          this.imageSrc = file_uri;
-          this.imageSelected = true;
-          console.log("hello");
+    this.platform.ready().then(() => {
+
+      this.library = [];
+
+      this.photoLibrary.getLibrary({ thumbnailWidth: THUMBNAIL_WIDTH, thumbnailHeight: THUMBNAIL_HEIGHT/*, chunkTimeSec: 0.3*/ }).subscribe({
+        next: (chunk) => {
+          this.library = this.library.concat(chunk);
+          //this.library = this.library.slice(0, 9); // To take top 10 images
+          this.cd.detectChanges();
+        },
+        error: (err: string) => {
+          if (err.startsWith('Permission')) {
+            let permissionsModal = this.modalCtrl.create(HomePage); // TODO: PermissionsPage
+            permissionsModal.onDidDismiss(() => {
+              this.fetchPhotos();
+            });
+            permissionsModal.present();
+          } else {
+            let toast = this.toastCtrl.create({
+              message: `getLibrary error: ${err}`,
+              duration: 6000,
+            });
+            toast.present();
+          }
         }
-      )
-      .catch(
-        function() {
-          console.log("Promise Rejected");
-        }
-      );
+      });
+    });
   }
-
 }
