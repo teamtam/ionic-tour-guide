b504a21e98b054e2fd1ebebd77ec4c7a97ab40df
Benjamin Tam <ben@teamtam.com>
2017-08-22 11:27:26 +1000

    Added map markers

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { Places } from "../../places";
 
 declare var google;
 
@@ -27,16 +28,25 @@ export class MapPage {
 
     this.geolocation.getCurrentPosition().then((position) => {
 
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let latLng = new google.maps.LatLng(-37.813628, 144.963058);
+      //let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
       let mapOptions = {
         center: latLng,
-        zoom: 15,
+        zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
+      Places.map(place => (
+        new google.maps.Marker({
+          map: this.map,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(place.lat, place.long),
+          title: place.name
+        })
+      ));
     }, (err) => {
       console.log(err);
     });
